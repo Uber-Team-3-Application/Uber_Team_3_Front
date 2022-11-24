464c8b0b86d284c6c6b67d6c08f1326aec758380
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-11-24 12:23:00 +0100

    Generated Components
    Navbar
    Login
    Registration
    About

--- Reesen/src/app/app.module.ts
+++ Reesen/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegistrationComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
