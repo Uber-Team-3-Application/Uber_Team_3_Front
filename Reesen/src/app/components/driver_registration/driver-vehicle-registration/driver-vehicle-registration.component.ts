6b6ebb70648e01fa897646c75d87ee02d76113a8
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2022-12-15 23:04:23 +0100

    added form for driver-vehicke-registration

---
+++ Reesen/src/app/components/driver_registration/driver-vehicle-registration/driver-vehicle-registration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-vehicle-registration',
+  templateUrl: './driver-vehicle-registration.component.html',
+  styleUrls: ['../driver_registration.component.css']
+})
+export class DriverVehicleRegistrationComponent {
+
+}
