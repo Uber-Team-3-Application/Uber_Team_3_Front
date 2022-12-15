4fd50dd051a095d541627ed5b05ff319de9799f1
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2022-12-15 23:04:02 +0100

    created form for driver-info-registration

---
+++ Reesen/src/app/components/driver_registration/driver-info-registration/driver-info-registration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-info-registration',
+  templateUrl: './driver-info-registration.component.html',
+  styleUrls: ['../driver_registration.component.css']
+})
+export class DriverInfoRegistrationComponent {
+
+}
