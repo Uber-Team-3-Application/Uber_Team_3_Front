cfcdd6b2fd4eb24d4fd234672903c4bf69747512
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-12-15 12:25:34 +0100

    Added styling to map component

--- Reesen/src/app/components/map/map.component.ts
+++ Reesen/src/app/components/map/map.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component } from '@angular/core';
 import * as L from 'leaflet';
 import { MapService } from 'src/app/services/map/map.service';
+import 'leaflet-routing-machine'
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -16,6 +17,17 @@ export class MapComponent implements AfterViewInit{
       center: [45.2396, 19.8227],
       zoom: 13,
     });
+
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+    tiles.addTo(this.map);
   }
 
   ngAfterViewInit(): void {
