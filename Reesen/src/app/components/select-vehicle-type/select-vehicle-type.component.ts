d2cce37dd66e1df63f50da8584a20cfa7d607b74
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-12-15 14:51:40 +0100

    Implemented getting vehicles from database

--- Reesen/src/app/components/select-vehicle-type/select-vehicle-type.component.ts
+++ Reesen/src/app/components/select-vehicle-type/select-vehicle-type.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VehicleType } from 'src/app/models/Vehicle';
+import { VehicleService } from 'src/app/services/vehicle/vehicle.service';
 
 @Component({
   selector: 'app-select-vehicle-type',
   templateUrl: './select-vehicle-type.component.html',
   styleUrls: ['./select-vehicle-type.component.css']
 })
-export class SelectVehicleTypeComponent {
+export class SelectVehicleTypeComponent implements OnInit{
+
+  vehicleTypes: VehicleType[];
+
+  constructor(private vehicleService: VehicleService){}
+  
+  ngOnInit():void{
+    this.vehicleService.getVehicleTypes()
+                        .subscribe(
+                          (vehicleTypes) => (this.vehicleTypes = vehicleTypes)
+                        );
+  }
 
 }
