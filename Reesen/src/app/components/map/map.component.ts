eaaa7b3691642a9db51130c0a21166cb803679b3
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-12-15 22:05:11 +0100

    Slight code refractor

--- Reesen/src/app/components/map/map.component.ts
+++ Reesen/src/app/components/map/map.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component } from '@angular/core';
 import * as L from 'leaflet';
+import 'leaflet-routing-machine';
 import { MapService } from 'src/app/services/map/map.service';
-import 'leaflet-routing-machine'
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { VehicleType } from 'src/app/models/Vehicle';
 import { VehicleService } from 'src/app/services/vehicle/vehicle.service';
@@ -18,10 +18,14 @@ export class MapComponent implements AfterViewInit{
   showGetRide: boolean = false;
   showVehicleType: boolean = false;
   vehicleTypes: VehicleType[];
+  typeSelected: boolean = false;
+  selectedVehicleName: String = '';
+  isFormValid:boolean = true;
+  isRideInfoOpened:boolean = false;
 
   getRideForm = new FormGroup({
-    departure: new FormControl('', [Validators.required]),
-    destination : new FormControl('', [Validators.required]),
+    departure: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    destination : new FormControl('', [Validators.required, Validators.minLength(3)]),
     babyTransport: new FormControl(false),
     petTransport: new FormControl(false)
   });
@@ -35,7 +39,6 @@ export class MapComponent implements AfterViewInit{
     });
 
    
-
     const tiles = L.tileLayer(
       'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       {
@@ -47,9 +50,12 @@ export class MapComponent implements AfterViewInit{
     );
     tiles.addTo(this.map);
 
-    //this.search();
-    //this.addMarker();
-    this.registerOnClick();
+  }
+
+  selectVehicleType(type:VehicleType):void{
+      this.selectedVehicleName = type.name;
+      this.showVehicleType = false;
+      this.typeSelected = true;
   }
 
   ngAfterViewInit(): void {
@@ -68,21 +74,26 @@ export class MapComponent implements AfterViewInit{
                         );
   }
 
-  search():void{
-    this.mapService.search('Radnicka 19, Novi Sad').subscribe(
+
+
+
+  search(address: string){
+    this.mapService.search(address).subscribe(
       {
         next: (result) =>{
-          console.log(result);
           L.marker([result[0].lat, result[0].lon])
-            .addTo(this.map)
-            .bindPopup('Pozdrav iz Radnicke 19')
-            .openPopup();
+          .addTo(this.map)
+          .bindPopup(address)
+          .openPopup();
+          
         },
         error:() =>{}
 
       }
     );
+    
   }
+
 
 
   addMarker():void{
@@ -94,8 +105,18 @@ export class MapComponent implements AfterViewInit{
         .openPopup();
   }
 
+
+
   route():void{
-      
+    
+    this.map.eachLayer((l) =>{
+      if( l instanceof L.Marker )
+        console.log(l);
+    });
+    //L.Routing.control({
+   //   waypoints:[L.latLng(2, 2), L.latLng(2, 2)]
+    //}).addTo(this.map);
+    
   }
 
   registerOnClick(): void{
@@ -112,14 +133,39 @@ export class MapComponent implements AfterViewInit{
         alert(mp.getLatLng());
     });
   }
-  getRide():void{
-      this.showGetRide = !this.showGetRide;
+
+  openGetRide():void{
+
+    this.showGetRide = true;
+
   }
+  getRide():void{
+    
+      
+    if(!this.getRideForm.valid){
+      this.isFormValid = false;
+      return;
+    }
+    if(this.selectedVehicleName === ''){
+      this.isFormValid = false;
+      return;
+    }
+    this.isFormValid = true;
+   
+    
+    this.search(this.getRideForm.value.departure);
+    this.search(this.getRideForm.value.destination);
+    document.getElementById("map").focus();
+    this.route();
+    this.showGetRide = false;
 
 
+
+  }
 
   openVehicleTypeComponent():void{
     this.showVehicleType = !this.showVehicleType; 
+    this.typeSelected = false;
   }
 
 }
