4cce3b4c8547876801499a6b0c076bd4d0de9a89
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-12-22 17:38:38 +0100

    added interceptor

--- Reesen/src/app/modules/auth/auth.module.ts
+++ Reesen/src/app/modules/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { LoginComponent } from './login/login.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
   ],
   exports:[LoginComponent]
 })
---
+++ Reesen/src/app/modules/auth/interceptor/interceptor.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class InterceptorInterceptor implements HttpInterceptor {
+
+
+  intercept(
+    request: HttpRequest<any>, 
+    next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+      const accessToken: any = localStorage.getItem('user');
+      const decodedItem = JSON.parse(accessToken);
+      if(request.headers.get('skip')) return next.handle(request);
+
+      if(accessToken){
+        const cloned = request.clone({
+          headers: request.headers.set('X-Auth-Token', decodedItem.token),
+
+        });
+        return next.handle(cloned);
+      }
+      return next.handle(request);
+      
+  }
+}
