7fbbd38f25b9541b21b0fc5c259774df893987b0
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-01-01 22:49:36 +0100

    added driver card history

---
+++ Reesen/src/app/modules/driver/driver-card-history-responsive/driver-card-history-responsive.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-card-history-responsive',
+  templateUrl: './driver-card-history-responsive.component.html',
+  styleUrls: ['./driver-card-history-responsive.component.css']
+})
+export class DriverCardHistoryResponsiveComponent {
+
+}
