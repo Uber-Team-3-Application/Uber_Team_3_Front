78dceedadf6988040e08d62eae0e04f7a9fb7c3f
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2023-01-03 10:36:21 +0100

    Added cards and loading into them

--- Reesen/src/app/modules/admin/user-ride-history/user-ride-history.component.ts
+++ Reesen/src/app/modules/admin/user-ride-history/user-ride-history.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Ride } from 'src/app/models/Ride';
+import { Ride, Review, SingleReview } from 'src/app/models/Ride';
 import { UserService } from '../../unregistered-user/user.service';
 
 @Component({
@@ -13,20 +13,24 @@ export class UserRideHistoryComponent implements OnInit{
   userId: number;
   userRole:string;
   rides: Ride[];
-
+  ratings = new Array();
+  hasLoaded: boolean = false;
   constructor(private userService: UserService, 
             private router: Router,
             private route: ActivatedRoute){}
 
   ngOnInit(): void {
+     this.hasLoaded = false;
      this.getDataFromUrl();
-
      this.userService.getRides(this.userId, 0, 5, 'timeOfStart', null, null)
           .subscribe(
           {
             next: (result) =>{
                 this.rides = result.results;
                 console.log(this.rides);
+                this.setRatings();
+                this.hasLoaded = true;
+                
             },
             error: (error) =>{
               this.rides = [];
@@ -41,6 +45,29 @@ export class UserRideHistoryComponent implements OnInit{
       let id = this.route.snapshot.paramMap.get('id');
       this.userId = +id;
       this.userRole = this.route.snapshot.paramMap.get('role');
+  }
+
+  private setRatings(): void{
+    for(let i=0;i<this.rides.length;i++){
+        let reviews: Review[] = this.rides[i].reviews;
+        if(reviews.length === 0) 
+        {
+          this.ratings[i] = 0; 
+          continue;
+
+        }
+        let totalNumberOfReviews: number = reviews.length  * 2;
+        let totalReviewScore: number = 0;
+        for(let j =0;j<reviews.length;j++){
+          let vehicleReview = reviews[i].vehicleReview;
+          let driverReview = reviews[i].driverReview; 
+          totalReviewScore += vehicleReview.rating;
+          totalReviewScore += driverReview.rating;
+        }
+      
+        this.ratings.push(totalReviewScore/totalNumberOfReviews);
+
+    }
   }
 
 }
