411f45111758447d704a0109238e4cc36906e001
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-01-08 16:48:32 +0100

    added driver card history responsive component

--- Reesen/src/app/modules/driver/driver-card-history/driver-card-history.component.ts
+++ Reesen/src/app/modules/driver/driver-card-history/driver-card-history.component.ts
@@ -13,11 +13,23 @@ import { BarRatingModule } from "ngx-bar-rating";
 export class DriverCardHistoryComponent {
 
   ride : Ride;
-  rating : number = 4;
+  rating : number = 1;
 
   @Input() set date(value : Ride) {
     this.ride = value;
+    let sum = 0;
+    let q = 1;
+
+    if (this.ride.reviews != undefined) {
+      for (let review of this.ride.reviews) {
+        sum += review.driverReview.rating;
+        sum += review.vehicleReview.rating;
+        q += 2;
+      }
+      this.rating = sum / q;
+    }
   }
+
 
 
 
