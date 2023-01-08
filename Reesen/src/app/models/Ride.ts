a0d451585f1496b93226788f5598bc272b1791cd
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-01-08 16:46:35 +0100

    added sorters for rides and modified ride interface

--- Reesen/src/app/models/Ride.ts
+++ Reesen/src/app/models/Ride.ts
@@ -2,21 +2,28 @@ import {Location, Route} from "./Location";
 import {Driver} from "./Driver";
 import {Passenger} from "./Passenger";
 import {Rejection} from "./Rejection";
+import {Review, RideReview} from "./Review";
+import {User, UserRestrict} from "./User";
+
 
 export interface Ride {
-  id:number,
-  startTime:string,
-  endTime: string,
-  totalCost : number,
-  driver : Driver,
-  passengers : Passenger[],
-  estimatedTimeInMinutes: number,
-  vehicleType : string,
-  babyTransport : boolean,
-  petTransport :boolean,
-  rejection?: Rejection,
+  id:number;
+  startTime:string;
+  endTime: string;
+  totalCost : number;
+  driver : Driver;
+  passengers : Passenger[];
+  estimatedTimeInMinutes: number;
+  vehicleType : string;
+  babyTransport : boolean;
+  petTransport :boolean;
+  rejection?: Rejection;
   locations: Route[];
+  reviews?: Review[];
 }
+
+
+
 
 export interface RideInfoBody{
     locations: Location[],
@@ -34,4 +41,39 @@ export interface RideInfo{
 export interface RidePaginated {
   totalCount : number;
   results : Ride[]
+
+}
+
+//=====SORTERS=====//
+
+export const sortRideByPriceAscending = (a: Ride, b: Ride) => {
+  if (a.totalCost > b.totalCost)
+    return 1;
+  if (a.totalCost < b.totalCost)
+    return -1;
+  return 0;
+}
+
+export const sortRideByPriceDescending = (a: Ride, b: Ride) => {
+  if (a.totalCost > b.totalCost)
+    return -1;
+  if (a.totalCost < b.totalCost)
+    return 1;
+  return 0;
+}
+
+export const sortRideByDateAscending = (a: Ride, b: Ride) => {
+  if (a.startTime > b.startTime)
+    return 1;
+  if (a.startTime < b.startTime)
+    return -1;
+  return 0;
+}
+
+export const sortRideByDateDescending = (a: Ride, b: Ride) => {
+  if (a.startTime > b.startTime)
+    return -1;
+  if (a.startTime < b.startTime)
+    return 1;
+  return 0;
 }
