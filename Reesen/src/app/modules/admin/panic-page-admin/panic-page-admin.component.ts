4321fd9bf614c5e90507c27fdfb5399d29d23da1
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2023-01-21 10:52:08 +0100

    Implemented panic service

--- Reesen/src/app/modules/admin/panic-page-admin/panic-page-admin.component.ts
+++ Reesen/src/app/modules/admin/panic-page-admin/panic-page-admin.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PanicDTO } from 'src/app/models/Panic';
+import { PanicService } from './panic.service';
 
 @Component({
   selector: 'app-panic-page-admin',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./panic-page-admin.component.css']
 })
 export class PanicPageAdminComponent {
+
+  userId: number;
+  userRole:string;
+  panics: PanicDTO[];
+  ratings = new Array();
+  hasLoaded = false;
+  selectedSort = "Start Time";
+  page = 1;
+  selectedShowNumber = 2;
+  totalEntries = 0;
+
+  constructor(private panicService: PanicService, 
+            private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+     this.hasLoaded = false;
+     this.getDataFromUrl();
+     this.getPanic();
+  }
+
+  private getPanic() {
+
+  
+  }
+
+
+
+  private getDataFromUrl():void{
+      const id = this.route.snapshot.paramMap.get('id');
+      this.userId = +id;
+      this.userRole = this.route.snapshot.paramMap.get('role');
+  }
+
 
 }
--- Reesen/src/app/modules/admin/panic-page-admin/panic.service.ts
+++ Reesen/src/app/modules/admin/panic-page-admin/panic.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PaginatedPanic } from 'src/app/models/Panic';
+import { environment } from 'src/app/environment/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PanicService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  
+  get():Observable<PaginatedPanic>{
+    return this.http.get<PaginatedPanic>(environment.apiHost + 'api/panic');
+  }
 }
