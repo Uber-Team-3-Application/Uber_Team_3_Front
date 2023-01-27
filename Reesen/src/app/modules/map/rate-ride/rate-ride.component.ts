382a16f6102db49a734f9a444808029b264a83a0
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2023-01-27 15:34:54 +0100

    Added basic design

--- Reesen/src/app/modules/map/rate-ride/rate-ride.component.ts
+++ Reesen/src/app/modules/map/rate-ride/rate-ride.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -10,6 +11,12 @@ export class RateRideComponent implements OnInit{
 
   rideId: number;
   id: number;
+  reviewForm = new FormGroup({
+    driverRating: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(13)]),
+    vehicleRating: new FormControl('', [Validators.required, Validators.email]),
+    driverComment: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    vehicleComment: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
 
   constructor(private router: Router,
             private route: ActivatedRoute){}
@@ -18,4 +25,7 @@ export class RateRideComponent implements OnInit{
     
   }
 
+  leaveReview(): void{
+
+  }
 }
