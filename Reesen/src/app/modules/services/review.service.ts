7085eb2c0143e2215614cb9f908ed555a2788b4f
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2023-01-27 15:11:54 +0100

    Added endpoints for leaving reviews

--- Reesen/src/app/modules/services/review.service.ts
+++ Reesen/src/app/modules/services/review.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/app/environment/environment';
+import { ReviewDTO } from 'src/app/models/Ride';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  leaveReviewForVehicle(rideId: number, rating: number, comment: number): Observable<ReviewDTO> {
+    let review = {
+      rating: rating,
+      comment:comment
+
+    };
+    return this.http.post<ReviewDTO>(environment.apiHost + "api/ride/", review);
+  }
+
+  leaveReviewForDriver(rideId: number, rating: number, comment: number): Observable<ReviewDTO> {
+    let review = {
+      rating: rating,
+      comment:comment
+
+    };
+    return this.http.post<ReviewDTO>(environment.apiHost + "api/ride/", review);
+  }
 }
