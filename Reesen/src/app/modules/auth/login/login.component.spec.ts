a7d075de8705462f8794056d7c31ad55be695f9e
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-02-06 19:41:20 +0100

    Added tests for user service

--- Reesen/src/app/modules/auth/login/login.component.spec.ts
+++ Reesen/src/app/modules/auth/login/login.component.spec.ts
@@ -1,5 +1,4 @@
 import { CommonModule } from '@angular/common';
-import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
@@ -9,7 +8,7 @@ import { AuthenticationService } from '../authentication.service';
 
 import { LoginComponent } from './login.component';
 
-fdescribe('LoginComponent', () => {
+describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let el: HTMLElement;
