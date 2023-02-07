9e074de326f5f17b8b275b2ea427e03d9ac9bc6a
Jelena Mikovic <jelena.mikovic189@gmail.com>
2023-02-07 20:07:22 +0100

    Added passenger service tests

--- Reesen/src/app/modules/passenger/passenger.service.spec.ts
+++ Reesen/src/app/modules/passenger/passenger.service.spec.ts
@@ -96,5 +96,126 @@ describe('PassengerService', () => {
     expect(req.request.method).toEqual('GET');
   })
 
+  it('should call findByEmail and return passenger', () => {
+    const testEmail = 'test@example.com';
+    const testPassengerId = 1;
+    const mockPassenger: Passenger = {
+      id: testPassengerId,
+      name: 'John',
+      surname: 'Doe',
+      profilePicture: 'https://example.com/profile-picture.jpg',
+      telephoneNumber: '+1 123 456 7890',
+      email: 'test@example.com',
+      address: '123 Main Street, Anytown USA 12345',
+      password: 'password123',
+      blocked: false,
+      active: true,
+      isConfirmedEmail: true,
+      amountOfMoney: 100.0,
+    };
+
+    service.findByEmail(testEmail).subscribe(passenger => {
+      expect(passenger).toEqual(mockPassenger);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiHost}api/passenger/${testEmail}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockPassenger);
+  });
+
+  it('should call get and return passenger', () => {
+    const testPassengerId = 1;
+    const mockPassenger: Passenger = {
+      id: testPassengerId,
+      name: 'John',
+      surname: 'Doe',
+      profilePicture: 'https://example.com/profile-picture.jpg',
+      telephoneNumber: '+1 123 456 7890',
+      email: 'john.doe@example.com',
+      address: '123 Main Street, Anytown USA 12345',
+      password: 'password123',
+      blocked: false,
+      active: true,
+      isConfirmedEmail: true,
+      amountOfMoney: 100.0,
+    };
+
+    service.get(testPassengerId).subscribe(passenger => {
+      expect(passenger).toEqual(mockPassenger);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiHost}api/passenger/${testPassengerId}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockPassenger);
+  });
+
+  it('should call activatePassenger and return string for activation', () => {
+    const testPassengerId = 1;
+    const mockResponse = 'Passenger activated';
+
+    service.activatePassenger(testPassengerId).subscribe(response => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiHost}api/passenger/activate/${testPassengerId}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse, { status: 200, statusText: 'OK' });
+  });
+
+  it('should call activateAccount and return url', () => {
+    const testUrl = 'https://example.com/activate/account';
+    const mockResponse = 'Account activated';
+
+    service.activateAccount(testUrl).subscribe(response => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiHost}api/passenger/activate/account?url=${testUrl}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse, { status: 200, statusText: 'OK' });
+  });
+
+  it('should return a paginated list of rides for a given passenger', () => {
+    const passengerId = 2;
+    const page = 1;
+    const size = 1;
+    const sort = 'asc';
+    const from = '2022-01-01';
+    const to = '2022-12-31';
+
+    service.getRidesOfPassenger(passengerId, sort, from, to, page, size).subscribe(rides => {
+      expect(rides).toBeTruthy();
+      expect(rides.totalCount).toEqual(1);
+      expect(rides.results.length).toEqual(1);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiHost}api/passenger/${passengerId}/ride?sort=${sort}&from=${from}&to=${to}&page=${page}&size=${size}`
+    );
+    expect(req.request.method).toEqual('GET');
+
+    req.flush({
+      totalCount: 1,
+      results: [
+        {
+          status: 'completed',
+          id: 1,
+          startTime: '2022-01-01T12:00:00',
+          endTime: '2022-01-01T12:30:00',
+          totalCost: 15,
+          driver: { id: 1, name: 'John Doe' },
+          passengers: [{ id: 2, name: 'Jane Doe' }],
+          estimatedTimeInMinutes: 30,
+          vehicleType: 'STANDARD',
+          babyTransport: false,
+          petTransport: false,
+          locations: [{ lat: 37.7749, lng: -122.4194 }],
+          reviews: [{ id: 1, rating: 5, comment: 'Great ride!' }],
+          scheduledTime: '2022-01-01T12:00:00'
+        }
+      ]
+    });
+  });
+
 
 })
